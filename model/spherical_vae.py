e9be7cc12aa39ed27a43bf6df41c64565feb7816
KHOUTAIBI <khoutaibi.iliass@gmail.com>
2025-11-20 21:09:04 +0100

    Training added

--- model/spherical_vae.py
+++ model/spherical_vae.py
@@ -1,7 +1,6 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-import geomstats as gs
 import numpy as np
 from sampler.spherical_uniform import *
 from sampler.von_mises_fisher import *
@@ -9,47 +8,42 @@ from sampler.von_mises_fisher import *
 
 class SphericalVAE(nn.Module):
     def __init__(self,
-                config, 
-                extrinsic_dim,
-                latent_dim,
-                sftbeta=4.5,
-                encoder_width=400,
-                encoder_depth=4,
-                decoder_width=400,
-                decoder_depth=4,
-                dropout_p=0.0,          
+                config,           
                 *args, 
                 **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         # Variables
         self.config = config
-        self.extrinsic_dim = extrinsic_dim
-        self.latent_dim = latent_dim
-        self.sftbeta = sftbeta
-        self.encoder_width=encoder_width
-        self.encoder_depth=encoder_depth
-        self.decoder_width=decoder_width
-        self.decoder_depth=decoder_depth
-        self.dropout_p=dropout_p
+        self.extrinsic_dim = config["extrinsic_dim"]
+        self.latent_dim = config["latent_dim"]
+        self.sftbeta = config["sftbeta"]
+        self.encoder_width=config["encoder_width"]
+        self.encoder_depth=config["encoder_depth"]
+        self.decoder_width=config["decoder_width"]
+        self.decoder_depth=config["decoder_depth"]
+        self.dropout_p=config["dropout_p"]
+        self.device = config["device"]
+        self.encoder_depth = config["encoder_depth"]
+        self.decoder_depth = config["decoder_depth"]
 
         # Encoder layers
-        self.encoder_function = nn.Linear(in_features=self.extrinsic_dim, out_features=self.encoder_width)
+        self.encoder_function = nn.Linear(in_features=self.extrinsic_dim, out_features=self.encoder_width, device=self.device)
         self.encoder_linear_layers = nn.ModuleList(
-            [nn.Linear(in_features=self.encoder_width, out_features=self.encoder_width) for _ in range(encoder_depth)]
+            [nn.Linear(in_features=self.encoder_width, out_features=self.encoder_width, device=self.device) for _ in range(self.encoder_depth)]
         )
         
-        self.function_z_mu = nn.Linear(in_features=self.encoder_width, out_features=self.latent_dim) # mean of the Von Mises Fischer
-        self.function_z_logvar = nn.Linear(in_features=self.encoder_width, out_features=1) # Kappa of Von Mises Fischer
+        self.function_z_mu = nn.Linear(in_features=self.encoder_width, out_features=self.latent_dim, device=self.device) # mean of the Von Mises Fischer
+        self.function_z_logvar = nn.Linear(in_features=self.encoder_width, out_features=1, device=self.device) # Kappa of Von Mises Fischer
 
         # Decoder layers
-        self.decoder_function = nn.Linear(in_features=self.latent_dim, out_features=decoder_width)
+        self.decoder_function = nn.Linear(in_features=self.latent_dim, out_features=self.decoder_width, device=self.device)
         self.decoder_linear_layers = nn.ModuleList(
-            [nn.Linear(in_features=self.decoder_width, out_features=self.decoder_width) for _ in range(decoder_depth)]
+            [nn.Linear(in_features=self.decoder_width, out_features=self.decoder_width, device=self.device) for _ in range(self.decoder_depth)]
         )
 
         # Posteriori
-        self.function_x_mu = nn.Linear(in_features=self.decoder_width, out_features=self.extrinsic_dim)
+        self.function_x_mu = nn.Linear(in_features=self.decoder_width, out_features=self.extrinsic_dim, device=self.device)
         self.dropout = nn.Dropout(p = self.dropout_p)
 
     
@@ -94,7 +88,8 @@ class SphericalVAE(nn.Module):
             Reconstructed data
 
         """
-
+        
+        z = self.decoder_function(z)
         h = F.softplus(z, beta = self.sftbeta)
 
         for layer in self.decoder_linear_layers:
@@ -132,11 +127,29 @@ class SphericalVAE(nn.Module):
         q_z = VonMisesFisher3D(z_mu, z_kappa)
         p_z = SphericalUniform(dim = self.latent_dim - 1, device=x.device)
 
-        KL = torch.distributions.kl.kl_divergence(q_z, p_z)
+        kappa = q_z.scale  # or q_z.kappa
+        kl_per_sample = self.kl_vmf_spherical_uniform(kappa)
+        KL = kl_per_sample.mean()
+        
         reconstruction_loss = F.mse_loss(x, x_mu)
 
         return reconstruction_loss + self.config["beta"] * KL
 
+
+    def kl_vmf_spherical_uniform(self, kappa) -> torch.Tensor:
+        """
+        KL( vMF(mu, kappa) || Uniform(S^2) )
+        assuming dimension = 3 (sphere S^2).
+        kappa: (...,) tensor of concentrations
+        returns: (...,) tensor of KLs
+        """
+        eps = 1e-8
+        # coth(kappa) = cosh(kappa) / (sinh(kappa) + eps)
+        sinh_k = torch.sinh(kappa) + eps
+        coth_k = torch.cosh(kappa) / sinh_k
+
+        kl = torch.log(kappa + eps) - torch.log(sinh_k) + kappa * coth_k - 1.0
+        return kl
     
     
 
