074bd4e44a99e37135d2823a0eb6fde4859a6c47
Yacine <yacine.khalil@telecom-paris.fr>
2025-11-23 20:20:50 +0100

    add cnn experiment

---
+++ CNN_experiment/data_generation.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms.functional as TF
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from tqdm import tqdm
+
+
